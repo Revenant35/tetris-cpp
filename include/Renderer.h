81526f4baaa034299617eaf1a4c124284172c099
zach brown <bbrown64506@gmail.com>
2025-09-12 14:59:39 -0500

    sprite work

--- include/Renderer.h
+++ include/Renderer.h
@@ -1,22 +1,33 @@
 #pragma once
 
-#include "Spritesheet.h"
-#include <SDL2/SDL.h>
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
 
-namespace Tetris {
+namespace Tetris
+{
     class Renderer {
     public:
-        Renderer();
+        constexpr static int SCREEN_WIDTH = 640;
+        constexpr static int SCREEN_HEIGHT = 480;
+        constexpr static int IMG_FLAGS = IMG_INIT_PNG;
 
-        void drawSprite(
-            const Spritesheet &sheet,
-            SDL_Rect *position,
-            int row,
-            int column
-        );
+        Renderer();
+        ~Renderer();
+
+        SDL_Texture *loadTexture(const std::string &path) const;
+        void freeTexture(SDL_Texture *texture) const;
+
+        void clear() const;
+        void drawTexture(SDL_Texture *texture, const SDL_Rect *src, const SDL_Rect *dest) const;
+        void present() const;
+
+        bool drawFilledRect(const SDL_Rect &rect, const SDL_Color &color) const;
+        bool drawOutlinedRect(const SDL_Rect &rect, const SDL_Color &color) const;
+        bool drawPoints(const SDL_Point *points, int count, const SDL_Color &color) const;
+
     private:
-        SDL_Window *window;
-        SDL_Surface *windowSurface;
+        SDL_Window* window;
+        SDL_Renderer* renderer;
     };
 }
-
--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -1,37 +1,111 @@
-#include "../include/Renderer.h"
-
-#include "Log.h"
+#include "Renderer.h"
+#include <stdexcept>
+#include <SDL_image.h>
 
 Tetris::Renderer::Renderer() {
-    window = SDL_CreateWindow(
-        "Pong",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
-        SDL_WINDOW_SHOWN
-    );
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        throw std::runtime_error("Failed to initialize SDL");
+    }
 
-    TETRIS_ASSERT(window != nullptr, "Failed to create window: {}", SDL_GetError());
+    window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                              SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        throw std::runtime_error("Failed to create window");
+    }
 
-    windowSurface = SDL_GetWindowSurface(window);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        throw std::runtime_error("Failed to create renderer");
+    }
 
-    TETRIS_ASSERT(windowSurface != nullptr, "Failed to get window surface: {}", SDL_GetError());
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        throw std::runtime_error("Failed to initialize SDL_image");
+    }
 }
 
-void Tetris::Renderer::drawSprite(
-    const Spritesheet &sheet,
-    SDL_Rect *position,
-    const int row,
-    const int column
-) {
-    const auto sheetSurface = sheet.getSurface();
-    TETRIS_ASSERT(sheetSurface != nullptr, "Spritesheet surface is null");
+Tetris::Renderer::~Renderer() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
 
-    SDL_Rect clip;
-    clip.w = sheetSurface->w / sheet.getColumnCount();
-    clip.h = sheetSurface->h / sheet.getRowCount();
-    clip.x = row * clip.w;
-    clip.y = column * clip.h;
-    SDL_BlitSurface(sheetSurface, &clip, windowSurface, position);
+    IMG_Quit();
+    SDL_Quit();
+}
+
+SDL_Texture* Tetris::Renderer::loadTexture(const std::string &path) const {
+    SDL_Surface *loadedSurface = IMG_Load(path.c_str());
+    if (loadedSurface == nullptr) {
+        printf("Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+        return nullptr;
+    }
+
+    SDL_Texture *newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+    SDL_FreeSurface(loadedSurface);
+
+    if (newTexture == nullptr) {
+        printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+        return nullptr;
+    }
+
+    return newTexture;
+}
+
+void Tetris::Renderer::freeTexture(SDL_Texture *texture) const {
+    SDL_DestroyTexture(texture);
+}
+
+void Tetris::Renderer::clear() const {
+    SDL_RenderClear(renderer);
+}
+
+void Tetris::Renderer::drawTexture(SDL_Texture *texture, const SDL_Rect *src, const SDL_Rect *dest) const {
+    SDL_RenderCopy(renderer, texture, src, dest);
+}
+
+void Tetris::Renderer::present() const {
+    SDL_RenderPresent(renderer);
+}
+
+bool Tetris::Renderer::drawFilledRect(const SDL_Rect &rect, const SDL_Color &color) const {
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+
+    if (SDL_RenderFillRect(renderer, &rect) != 0) {
+        printf("Unable to draw filled rect! SDL Error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+bool Tetris::Renderer::drawOutlinedRect(const SDL_Rect &rect, const SDL_Color &color) const {
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+    SDL_Point points[5];
+    points[0] = {rect.x, rect.y};
+    points[1] = {rect.x + rect.w, rect.y};
+    points[2] = {rect.x + rect.w, rect.y + rect.h};
+    points[3] = {rect.x, rect.y + rect.h};
+    points[4] = {rect.x, rect.y};
+
+    if (SDL_RenderDrawLines(renderer, points, 5) != 0) {
+        printf("Unable to draw outlined rect! SDL Error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+bool Tetris::Renderer::drawPoints(const SDL_Point* points, const int count, const SDL_Color &color) const {
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+
+    bool success = true;
+    for (int i = 0; i < count; ++i) {
+        if (SDL_RenderDrawPoint(renderer, points[i].x, points[i].y) == 0) {
+            printf("Unable to draw point! SDL Error: %s\n", SDL_GetError());
+            success = false;
+        }
+    }
+    return success;
 }
