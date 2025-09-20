a3f461ce55a16b4cbad58bbf4f31706d0de94c04
zach brown <bbrown64506@gmail.com>
2025-09-19 20:20:02 -0500

    Revert "refactor: cleaning up Font"
    This reverts commit e6418bc4486274c890fd58bef8aed6758f13d6f5.

--- Core/src/Font.cpp
+++ Core/src/Font.cpp
@@ -3,28 +3,9 @@
 #include "Log.h"
 
 Core::Font::Font(const std::string &path, const int fontSize) {
-    TTF_Font *font = TTF_OpenFont(path.c_str(), fontSize);
-    if (font == nullptr) {
-        throw std::runtime_error("Failed to load font: " + path + ". SDL_ttf error: " + TTF_GetError());
-    }
-
-    m_Font.reset(font);
-    m_FontSize = TTF_FontHeight(m_Font.get());
+    m_Font = TTF_OpenFont(path.c_str(), fontSize);
 }
 
-Core::Font::Font(Font &&other) noexcept : m_Font(std::move(other.m_Font)),
-                                          m_FontSize(other.m_FontSize) {
-    other.m_FontSize = 0;
-}
-
-Core::Font &Core::Font::operator=(Font &&other) noexcept {
-    if (this == &other) {
-        return *this;
-    }
-
-    m_Font = std::move(other.m_Font);
-    m_FontSize = other.m_FontSize;
-    other.m_FontSize = 0;
-
-    return *this;
+Core::Font::~Font() {
+    TTF_CloseFont(m_Font);
 }
