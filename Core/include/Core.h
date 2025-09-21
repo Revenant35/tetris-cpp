327c71a078392def2c521733679b3f4bf314c776
zach brown <bbrown64506@gmail.com>
2025-09-21 08:05:45 -0500

    feat: precompiled header

--- Core/include/Core.h
+++ Core/include/Core.h
@@ -1,15 +1,6 @@
 #pragma once
 
-#include <string>
-#include <functional>
-#include <sstream>
-#include <cstdint>
-#include <memory>
-#include <vector>
-#include <unordered_map>
-#include <stdexcept>
-#include <fstream>
-#include <variant>
+#include <pch.h>
 
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
---
+++ Core/include/pch.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+#include <functional>
+#include <sstream>
+#include <cstdint>
+#include <memory>
+#include <vector>
+#include <unordered_map>
+#include <stdexcept>
+#include <fstream>
+#include <variant>
